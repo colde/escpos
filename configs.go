ed2c2dcfb5f32aa9570f0b522e05f6aff0813eaf
Loke Dupont <lodu@tv2.dk>
2024-11-16 14:00:04 +0100

    Add Xprinter to configs

--- configs.go
+++ configs.go
@@ -4,4 +4,5 @@ var (
 	ConfigEpsonTMT20II = PrinterConfig{}
 	ConfigEpsonTMT88II = PrinterConfig{DisableUpsideDown: true}
 	ConfigSOL802       = PrinterConfig{DisableUpsideDown: true}
+	ConfigXprinterT80Q = PrinterConfig{}
 )
